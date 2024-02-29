fca2f653e515d6aeadc49d43c760845f66212c94
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-02-29 21:05:11 +0530

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,17 @@
+print(10>9)
+
+a=100
+b=90
+if a>b:
+    print("a")
+else:
+    print("b")
+
+# these values are only false in bool 
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
