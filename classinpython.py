f45364ba749373a76f6abfc560887446dc32b97f
Apoorva Ranjeet <105016585+ApoorvaRanjeet@users.noreply.github.com>
2024-03-02 10:05:42 +0530

    Add files via upload

---
+++ classinpython.py
@@ -0,0 +1,60 @@
+# class Myclass():
+#     x=5
+
+# object = Myclass()
+# print(object.x)
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("John", 36)
+
+# print(p1.name)
+# print(p1.age)
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def __str__(self):
+#     return f"{self.name}({self.age})"
+
+# p1 = Person("John", 36)
+
+# print(p1)
+
+# class Person:
+#   def __init__(mysillyobject, name, age):
+#     mysillyobject.name = name
+#     mysillyobject.age = age
+
+#   def myfunc(abc):
+#     print("Hello my name is " + abc.name)
+
+# p1 = Person("John", 36)
+# p1.myfunc()
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+# x = Person("John", "Doe")
+# x.printname()
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+ def printname(self):
+    print(self.fname, self.lname)
+
+x = Student("John", "Doe")
+x.printname()
